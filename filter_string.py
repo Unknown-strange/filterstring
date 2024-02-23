1b4b32241cd3ad5149ee9f9c207d0b80c2aab9bd
The_Unknown <128997233+Unknown-strange@users.noreply.github.com>
2024-02-23 16:05:12 +0000

    Update filter_string.py

--- filter_string.py
+++ filter_string.py
@@ -3,5 +3,6 @@ def filter_string(words, char):
     new_word = [word for word in words]
     if char in new_word: new_word.remove(char)
     print(''.join(new_word))
-    
+
+# code test :)
 filter_string("Prince", "r")
